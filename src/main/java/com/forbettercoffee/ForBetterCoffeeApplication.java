c0d5f7a00397746eecd42450d9c9013261e3086c
iheese <imbc0224@naver.com>
2022-11-08 10:52:37 +0900

    start project

---
+++ src/main/java/com/forbettercoffee/ForBetterCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.forbettercoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForBetterCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForBetterCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forbettercoffee/ForBetterCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forbettercoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForBetterCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
